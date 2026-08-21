cf59f39b77881f761ff60f5d743082ec33b5d9b8
clipbotsahoj <3018788@edison.k12.nj.us>
2026-08-21 01:36:55 +0000

    Convert to ut-assert test suite, add APID range check test

--- local-tests/test_ccsds_raw_bytes.c
+++ local-tests/test_ccsds_raw_bytes.c
@@ -1,26 +1,43 @@
-#include <stdio.h>
-#include <assert.h>
 #include <string.h>
+#include "utassert.h"
+#include "uttest.h"
 #include "cfe_msg.h"
 
-int main(void) {
+void Test_APID_NoTruncation(void)
+{
     CFE_MSG_Message_t msg;
-
-    /* Zero out the message struct first */
     memset(&msg, 0, sizeof(msg));
 
-    /* Check 1: APID must not truncate */
     CFE_MSG_SetApId(&msg, 0x7FD);
-    uint16_t apid_bytes = ((msg.Byte[0] << 8) | msg.Byte[1]) & 0x07FF;
-    assert(apid_bytes == 0x7FD);
-    printf("PASS: APID round-trips as 0x%X\n", apid_bytes);
+    uint16 apid_bytes = ((msg.Byte[0] << 8) | msg.Byte[1]) & 0x07FF;
 
-    /* Check 2: Length must be size - 7 */
+    UtAssert_INT32_EQ(apid_bytes, 0x7FD);
+}
+
+void Test_APID_RangeChecking(void)
+{
+    CFE_MSG_Message_t msg;
+    memset(&msg, 0, sizeof(msg));
+
+    int32 result = CFE_MSG_SetApId(&msg, 0x800);
+
+    UtAssert_INT32_EQ(result, CFE_MSG_BAD_ARGUMENT);
+}
+
+void Test_Length_Convention(void)
+{
+    CFE_MSG_Message_t msg;
+    memset(&msg, 0, sizeof(msg));
+
     CFE_MSG_SetSize(&msg, 106);
-    uint16_t length_bytes = (msg.Byte[4] << 8) | msg.Byte[5];
-    assert(length_bytes == 99);
-    printf("PASS: Length encodes as %d (expected 99)\n", length_bytes);
+    uint16 length_bytes = (msg.Byte[4] << 8) | msg.Byte[5];
 
-    printf("All checks passed.\n");
-    return 0;
+    UtAssert_INT32_EQ(length_bytes, 99);
+}
+
+void UtTest_Setup(void)
+{
+    UtTest_Add(Test_APID_NoTruncation, NULL, NULL, "Test_APID_NoTruncation");
+    UtTest_Add(Test_APID_RangeChecking, NULL, NULL, "Test_APID_RangeChecking");
+    UtTest_Add(Test_Length_Convention, NULL, NULL, "Test_Length_Convention");
 }
